2361219c3f09cf72419dacf1b621887cb7859a1b
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-12-13 14:14:29 +1100

    satisfy Updates interface

--- monitor/github/rss.go
+++ monitor/github/rss.go
@@ -51,22 +51,26 @@ func NewWatched(store store.Storage, urls ...string) (*watched, error) {
 	return r, nil
 }
 
-// GetUnseen - get previously unseen releases
-func (r *watched) GetUnseen() ([][]string, error) {
-	r.update()
+func (w *watched) GetReleases() ([][]string, error) {
+	return w.GetUnseenReleases()
+}
+
+// GetUnseenReleases - get previously unseen releases
+func (w *watched) GetUnseenReleases() ([][]string, error) {
+	w.update()
 	unseen := [][]string{}
-	for i := range r.Feeds {
-		for j := range r.Feeds[i].Feed.Items {
-			title := r.Feeds[i].Feed.Items[j].Title
-			seen, err := r.Store.CheckExists(title)
+	for i := range w.Feeds {
+		for j := range w.Feeds[i].Feed.Items {
+			title := w.Feeds[i].Feed.Items[j].Title
+			seen, err := w.Store.CheckExists(title)
 			if err != nil {
 				log.Printf("unable to check %q with error %v", title, err)
 				return [][]string{}, fmt.Errorf("unable to check store with error %w", err)
 			}
 			if !seen {
-				content := r.Feeds[i].Feed.Items[j].Content
-				link := r.Feeds[i].Feed.Items[j].Link
-				if err := r.Store.CreateItem(title, content, link); err != nil {
+				content := w.Feeds[i].Feed.Items[j].Content
+				link := w.Feeds[i].Feed.Items[j].Link
+				if err := w.Store.CreateItem(title, content, link); err != nil {
 					log.Printf("unable to create %q with error %v", title, err)
 					return [][]string{}, fmt.Errorf("unable to create item with error %w", err)
 				}
@@ -78,20 +82,21 @@ func (r *watched) GetUnseen() ([][]string, error) {
 }
 
 // Update - fetch all items for all feeds
-func (r *watched) update() {
+func (w *watched) update() {
 	fp := gofeed.NewParser()
-	for idx := range r.Feeds {
-		feed, err := r.fetchURL(fp, r.Feeds[idx].URL)
+	for idx := range w.Feeds {
+		url := w.Feeds[idx].URL
+		feed, err := w.fetchURL(fp, url)
 		if err != nil {
-			log.Printf("error fetching url: %s, err: %v", r.Feeds[idx].URL, err)
+			log.Printf("error fetching url: %s, err: %v", url, err)
 		}
-		r.Feeds[idx].Feed = feed
+		w.Feeds[idx].Feed = feed
 	}
 }
 
 // FetchURL fetches the feed URL and also fakes the user-agent to be able
 // to retrieve data from sites like reddit.
-func (r *watched) fetchURL(fp *gofeed.Parser, url string) (feed *gofeed.Feed, err error) {
+func (w *watched) fetchURL(fp *gofeed.Parser, url string) (feed *gofeed.Feed, err error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", url, nil)
